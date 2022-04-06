e40ec924b01297ca10d28b1f3da687930dbf6376
GFrancV <gfrancv@hotmail.com>
2022-04-06 11:07:49 +0100

    The first part of the login is ready.

--- src/main.js
+++ src/main.js
@@ -7,18 +7,13 @@ import { createApp } from "vue"
 import App from "./App.vue"
 import router from "./router"
 import axios from "axios"
+import VueAxios from "vue-axios"
 
 //Notify plugin
 import Toaster from "vue-toast-notification"
 import "vue-toast-notification/dist/theme-sugar.css"
 
-const app = createApp(App).use(router).use(Toaster)
-
-//Change this URL to test
-const url = "http://10.20.139.17:40080"
-
-axios.defaults.baseURL = url
-app.config.globalProperties.$axios = axios
-app.config.globalProperties.$serverUrl = url
+axios.defaults.timeout = 55000
+const app = createApp(App).use(router).use(VueAxios, axios).use(Toaster)
 
 app.mount("#app")
--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -2,20 +2,54 @@
 	<div class="container signin">
 		<h1>Sign In</h1>
 		<div class="form-sign">
+			<label class="form-label" for="ipServer">OpenStack Server</label>
 			<div class="row">
-				<div class="col-sm-8">
-					<label class="form-label" for="ipServer">OpenStack Server</label>
-					<input v-model="credentials.ip" class="form-control" type="text" id="ipServer" required />
-				</div>
-				<div class="col-sm-4">
-					<label class="form-label" for="portServer">Port</label>
+				<div class="col-sm-2">
 					<input
-						v-model="credentials.port"
+						v-model="credentials.ip[0]"
 						class="form-control"
 						type="number"
-						id="portServer"
+						min="0"
+						max="255"
 						required
 					/>
+				</div>
+				.
+				<div class="col-sm-2">
+					<input
+						v-model="credentials.ip[1]"
+						class="form-control"
+						type="number"
+						min="0"
+						max="255"
+						required
+					/>
+				</div>
+				.
+				<div class="col-sm-2">
+					<input
+						v-model="credentials.ip[2]"
+						class="form-control"
+						type="number"
+						min="0"
+						max="255"
+						required
+					/>
+				</div>
+				.
+				<div class="col-sm-2">
+					<input
+						v-model="credentials.ip[3]"
+						class="form-control"
+						type="number"
+						min="0"
+						max="255"
+						required
+					/>
+				</div>
+				:
+				<div class="col-sm-3">
+					<input v-model="credentials.port" class="form-control" type="number" required />
 				</div>
 			</div>
 			<label class="form-label" for="username">User</label>
@@ -32,9 +66,6 @@
 			<button type="button" class="btn btn-primary px-5" @click="checkForm">Login</button>
 		</div>
 	</div>
-	<div v-if="errors" class="container">
-		{{ credentials }}
-	</div>
 </template>
 
 <script>
@@ -43,21 +74,95 @@
 		data() {
 			return {
 				credentials: {
-					ip: "",
-					port: "",
+					ip: {},
+					port: 80,
 					username: "",
 					password: "",
 				},
+				openstack: "",
 				errors: null,
+				message: {
+					auth: {
+						identity: {
+							methods: ["password"],
+							password: {
+								user: {
+									name: "demo",
+									domain: {
+										name: "Default",
+									},
+									password: "devstack",
+								},
+							},
+						},
+					},
+				},
 			}
 		},
 		methods: {
 			checkForm() {
-				if (this.credentials.ip == "") this.$toast.error("Ip of server is required!")
-				if (this.credentials.port == "") this.$toast.error("Port of server is required!")
+				//if (this.credentials.ip == "") this.$toast.error("Ip of server is required!")
+				//if (this.credentials.port == "") this.$toast.error("Port of server is required!")
 				if (this.credentials.username == "") this.$toast.error("Username is required!")
 				if (this.credentials.password == "") this.$toast.error("Password is required!")
-				if (this.credentials.password != "" && this.credentials.username != "") this.errors = true //this.login()
+				if (this.credentials.password != "" && this.credentials.username != "") this.login()
+			},
+
+			login() {
+				this.axios
+					.post(
+						"http://" +
+							this.credentials.ip[0] +
+							"." +
+							this.credentials.ip[1] +
+							"." +
+							this.credentials.ip[2] +
+							"." +
+							this.credentials.ip[3] +
+							":" +
+							this.credentials.port +
+							"/identity/v3/auth/tokens",
+						{
+							auth: {
+								identity: {
+									methods: ["password"],
+									password: {
+										user: {
+											name: this.credentials.username,
+											domain: {
+												name: "Default",
+											},
+											password: this.credentials.password,
+										},
+									},
+									scope: {
+										project: {
+											id: "default",
+										},
+									},
+								},
+							},
+						},
+						{
+							headers: {
+								"Content-Type": "application/json",
+							},
+						}
+					)
+					.then(response => {
+						this.user = response.data.token.user
+						this.user.token = response.headers["x-subject-token"]
+						this.user.scope = "project"
+						//this.getProjects()
+						this.loading = false
+						this.$toast.success("SignIn successful!")
+						console.log(response)
+					})
+					.catch(error => {
+						if (error.response.status == 401)
+							this.$toast.error("Failed to login! Wrong username or password")
+						this.loading = false
+					})
 			},
 		},
 	}
@@ -65,7 +170,7 @@
 
 <style>
 	.signin {
-		width: 500px;
+		width: 600px;
 	}
 
 	.form-sign {
